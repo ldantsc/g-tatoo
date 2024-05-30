5ec1286ea2cbfb05419d859fbe992bd4c951aed4
Lucas <dants.dev@gmail.com>
2024-05-29 21:01:26 -0300

    style: added icons on information page

--- src/app/pages/information/information.component.ts
+++ src/app/pages/information/information.component.ts
@@ -1,19 +1,37 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-information',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './information.component.html',
   styleUrl: './information.component.scss',
 })
 export class InformationComponent {
-  items = [
-    'Variedade de estilos',
-    'Preços transparentes',
-    'Horários flexíveis',
-    'Atendimento personalizado',
-    'Ambiente higiênico e seguro',
-    'Estilo único e personalizado',
+  items = [{
+    icon: 'palette_outlined',
+    descript: 'Variedade de estilos'
+  },
+  {
+    icon: 'paid',
+    descript: 'Preços transparentes'
+  },
+  {
+    icon: 'alarm_on',
+    descript: 'Horários flexíveis'
+  },
+  {
+    icon: 'handshake',
+    descript: 'Atendimento personalizado'
+  },
+  {
+    icon: 'sanitizer',
+    descript: 'Ambiente higiênico e seguro'
+  },
+  {
+    icon: 'star_half',
+    descript: 'Estilo único e personalizado'
+  },
   ];
 }
