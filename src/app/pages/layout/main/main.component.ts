d0694f6ad226d08e589a0fb0dcf4453c8a7bfb30
Lucas <dants.dev@gmail.com>
2024-05-30 13:07:17 -0300

    feat: added all contents

--- src/app/pages/layout/main/main.component.ts
+++ src/app/pages/layout/main/main.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { HomeComponent } from '../../home/home.component';
 import { QuoteComponent } from '../../quote/quote.component';
 import { AboutComponent } from '../../about/about.component';
-import { ContactComponent } from '../../contact/contact.component';
 import { HeaderComponent } from '../../../components/header/header.component';
 import { ButtonComponent } from '../../../components/button/button.component';
 import { RatingComponent } from '../../rating/rating.component';
 import { InformationComponent } from '../../information/information.component';
+import { FooterComponent } from '../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-main',
@@ -18,7 +18,7 @@ import { InformationComponent } from '../../information/information.component';
     InformationComponent,
     RatingComponent,
     AboutComponent,
-    ContactComponent,
+    FooterComponent,
     ButtonComponent,
   ],
   templateUrl: './main.component.html',
